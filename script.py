d9c3d166581a1b572905127cd4a21701249b0043
Việt Trung <89530449+vietrux@users.noreply.github.com>
2022-04-21 22:59:07 +0700

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-from tkb.models import Day                
-day1 = Day(day_name='Thứ 3', day_subj='vatly')
-day3 = Day(day_name='Thứ 3', day_subj='congnghe')
-day3 = Day(day_name='Thứ 3', day_subj='qpan')
-day4 = Day(day_name='Thứ 3', day_subj='nguvan')
-day5 = Day(day_name='Thứ 3', day_subj='nguvan')
-day_list = [ day2, day3, day4, day5]
-for x in day_list:
-    x.save()
